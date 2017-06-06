c0f3a8d1d5c0c678a9981ac2b79bd72bdb1a3b18
Alan Nise <alan@apium.com>
2017-06-05 18:47:13 -0700

    Added module sleep (copied form bridgeDataDiver)
    THis really needs to go into a submodule

---
+++ module/sleep.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   sleep.h
+ * Author: ahnise
+ *
+ * Created on January 26, 2017, 1:57 PM
+ */
+
+#ifndef SLEEP_H
+#define SLEEP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+void sleep_ms(int milliseconds);    
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SLEEP_H */
+
